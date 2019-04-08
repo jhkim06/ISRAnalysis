404c22e0cd528fb7ca4405b07a76bbdc7d6af683
Junho Kim <jhkim@cern.ch>
2019-04-09 02:14:07 +0900

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,2 @@
+export LD_LIBRARY_PATH=/home/jhkim/ISR2016/unfolding/TUnfoldISR2016/lib:$LD_LIBRARY_PATH
+export ROOT_INCLUDE_PATH=/home/jhkim/ISR2016/unfolding/TUnfoldISR2016/TUnfold:$ROOT_INCLUDE_PATH
