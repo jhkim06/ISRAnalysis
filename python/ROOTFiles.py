649b05389c1c25a75d479a37f54084c8ce8d7931
jhkim06 <lukek06@gmail.com>
2023-05-21 19:08:58 +0900

    add get_ratio_hist()

--- python/ROOTFiles.py
+++ python/ROOTFiles.py
@@ -25,7 +25,7 @@ class ROOTFiles:
         return stack_list
 
     # get values, bin, error as numpy array
-    def get_hist(self, file_key, hist_path, axis_steering=""):
+    def get_hist(self, file_key, hist_path, axis_steering="", root_hist=False):
         sample_type, hist_label, file_name = file_key.split("/")
         raw_hist = None
         if file_name == "all":  # sum of all histograms
@@ -39,12 +39,42 @@ class ROOTFiles:
             file_path = self.path+"/"+self.input_files[sample_type][hist_label][file_name]
             raw_hist = get_raw_hist(file_path, hist_path, axis_steering)
 
-        return root_to_numpy(raw_hist)
+        if root_hist:
+            return raw_hist
+        else:
+            return root_to_numpy(raw_hist)
+
+    def get_ratio_hist(self, nominator_file_key_list, denominator_file_key_list,
+                       nom_hist_path_list, denom_hist_path_list, axis_steering=""):
+
+        combined_nominator = self.get_combined_hist(nominator_file_key_list, nom_hist_path_list,
+                                                    axis_steering, root_hist=True)
+        combined_denominator = self.get_combined_hist(denominator_file_key_list, denom_hist_path_list,
+                                                      axis_steering, root_hist=True)
+
+        ratio = combined_nominator.Clone("ratio")
+        ratio.Divide(combined_denominator)
+        
+        return root_to_numpy(ratio)
+
+    def get_combined_hist(self, file_key_list, hist_path_list, axis_steering="", root_hist=False):
+        raw_hist = None
+        for index, file_key in enumerate(file_key_list):
+            hist = self.get_hist(file_key, hist_path_list[index], axis_steering, root_hist=True)
+            if index == 0:
+                raw_hist = hist
+            else:
+                raw_hist.Add(hist)
+
+        if root_hist:
+            return raw_hist
+        else:
+            return root_to_numpy(raw_hist)
 
     def get_raw_labels(self, file_key, hist_name):
         sample_type, hist_label, file_name = file_key.split("/")
         if file_name == "all":
-            file_name = [*self.input_files[sample_type][hist_label].keys()][0]
+            file_name = [*self.input_files[sample_type][hist_label].keys()][0]  # use one of samples
         file_path = self.path+"/"+self.input_files[sample_type][hist_label][file_name]
 
         return get_raw_labels(file_path, hist_name)
